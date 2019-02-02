8c883f6c0cd307b2c80f3ac4b11a94aac929e90c
Mathias Gebbe <Mathias.Gebbe@hellmann.com>
2019-02-02 23:51:29 +0100

    add bins

--- i3lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrot  'lockbg.png' -e 'convert -blur 0x4 $f ~/lockbg.png'
-convert -gravity center -composite ~/lockbg.png ~/lock.png ~/lockfinal.png
-i3lock -i ~/lockfinal.png
-#i3lock -i ~/lockbg.png
-rm ~/lockfinal.png ~/lockbg.png
