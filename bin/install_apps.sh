aa57f18b5d8fb3e9d478c66d3a4b5a9554a4cf10
Mathias Gebbe <Mathias.Gebbe@hellmann.com>
2020-02-21 08:54:52 +0100

    mod config

--- bin/install_apps.sh
+++ bin/install_apps.sh
@@ -3,10 +3,15 @@
 apt-get update
 
 for app in 
- vim ipcalc kazam shutter geany pinta tshark
+ curl vim ipcalc kazam shutter geany pinta tshark compton 
 do
     apt install $app
 done
 
 pip install yq
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "HISTSIZE=1000000000000" >> ~/.zshrc
+echo "SAVEHIST=1000000000000" >> ~/.zshrc
 
