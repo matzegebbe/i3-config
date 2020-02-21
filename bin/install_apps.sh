d61c5edc1e4b57b241cd53481509f508c9c138bb
Mathias Gebbe <Mathias.Gebbe@hellmann.com>
2020-02-21 09:10:21 +0100

    mod config

--- bin/install_apps.sh
+++ bin/install_apps.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 for app in 
- curl vim ipcalc kazam shutter geany pinta tshark compton 
+ curl vim ipcalc kazam shutter geany pinta tshark compton blueman bash-completion
 do
     apt install $app
 done
@@ -15,3 +15,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "HISTSIZE=1000000000000" >> ~/.zshrc
 echo "SAVEHIST=1000000000000" >> ~/.zshrc
 
+add-apt-repository ppa:yubico/stable
+apt-get install yubikey-manager-qt yubioath-desktop yubikey-personalization-gui
--- bin/install_minikube.sh
+++ bin/install_minikube.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
 sudo cp minikube /usr/local/bin && rm minikube
+
+echo 'source <(kubectl completion bash)' >>~/.zshrc
